eef90a3264788f4e6132faf07cd4b440c21c6e51
Jemiking <112058670+Jemiking@users.noreply.github.com>
2025-06-18 23:06:45 +0800

    修复: 解决Hilt重复绑定NotificationConfig的问题
    - 升级AGP从8.1.4到8.2.1，修复Java 21兼容性问题
    - 删除shared-notification模块中的NotificationConfigModule
    - 保留app模块中的NotificationConfig提供方法

--- shared/notification/src/main/kotlin/com/ccxiaoji/shared/notification/di/NotificationModule.kt
+++ shared/notification/src/main/kotlin/com/ccxiaoji/shared/notification/di/NotificationModule.kt
@@ -2,10 +2,8 @@ package com.ccxiaoji.shared.notification.di
 
 import com.ccxiaoji.shared.notification.api.NotificationApi
 import com.ccxiaoji.shared.notification.data.NotificationApiImpl
-import com.ccxiaoji.shared.notification.domain.model.NotificationConfig
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -20,21 +18,4 @@ abstract class NotificationModule {
     @Binds
     @Singleton
     abstract fun bindNotificationApi(impl: NotificationApiImpl): NotificationApi
-}
-
-/**
- * 提供NotificationConfig的模块
- * 这个需要在app模块中提供实际的配置
- */
-@Module
-@InstallIn(SingletonComponent::class)
-interface NotificationConfigModule {
-    companion object {
-        @Provides
-        @Singleton
-        fun provideNotificationConfig(): NotificationConfig {
-            // 这个方法会在app模块中被覆盖
-            throw NotImplementedError("NotificationConfig must be provided by app module")
-        }
-    }
 }
